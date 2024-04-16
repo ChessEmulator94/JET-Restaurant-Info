73d3d79637e3f54ba3bb85907189ed47c2c02d07
Gadi <guppiefriedman@gmail.com>
2024-04-16 19:35:34 +0200

    configure DB connection

---
+++ dbHandler.js
@@ -0,0 +1,20 @@
+import mysql from "mysql2";
+
+// Set to localhost
+const HOST = "127.0.0.1";
+// Set to DB username
+const USER = "root";
+// Set to DB password
+const PASSWORD = "rootroot";
+// Set to DB name
+const DATABASE = "";
+
+// Create a lasting connection to the DB
+const pool = mysql
+  .createPool({
+    host: HOST,
+    user: USER,
+    password: PASSWORD,
+    database: DATABASE,
+  })
+  .promise();
