03f5f72fe915ed4d327423737cd197bd8c4d0812
Gadi <guppiefriedman@gmail.com>
2024-04-18 22:19:50 +0200

    Processing of GET requests for specific post codes added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import bodyParser from "body-parser";
 import express, { json } from "express";
-//import cors from "cors";
+// Import DB handlers
+import { getRestaurants } from "./dbHandler.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 5500;
@@ -8,18 +10,17 @@ const PORT = 5500;
 app.use(bodyParser.json());
 app.use(express.json());
 
-// Enables CORS for ALL routes (not safe)
-//app.use(cors());
-
-// Import DB handlers
-import { getRestaurants } from "./dbHandler.js";
+//Enables CORS for ALL routes
+app.use(cors());
 
 // Import function to get data from api
 
 // Process GET request from http://localhost:${PORT}/
 app.get("/restaurants/:postCode", (req, res) => {
-  let foundRestaurants = getRestaurants(req.params.postCode);
-  res.send(foundRestaurants);
+  let postCode = req.params.postCode;
+  getRestaurants(postCode).then((response) => {
+    res.send(response[0]);
+  });
 });
 
 // Process POST request from http://localhost:${PORT}/
