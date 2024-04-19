1b5e41af6f5b2f6f417ad2efbc2fe0a0499aa450
Gadi <guppiefriedman@gmail.com>
2024-04-19 15:39:53 +0200

    updateView functionality added - dynamically adjust displayed listings

--- web_page/scripts/searchResultsScript.js
+++ web_page/scripts/searchResultsScript.js
@@ -1,6 +1,9 @@
 // Set equal to the URL server is running on (see server.js)
 const SERVER_URL = `http://localhost:5500`;
 
+// Offset the restaurants being shown
+let listingOffset = 0;
+
 /* When window loads, do thte following:
  *  - Extract searched postcode from the URL
  *  - Check if the postcode is in the database already
@@ -18,12 +21,20 @@ document.addEventListener("DOMContentLoaded", () => {
     if (exists === "false") {
       // if not, query JET API and add resulting listings to db
       console.log("OI VEY");
+      addRestaurantsToDb(postCode).then(
+        // Get the restaurant data
+        getRestaurants(postCode).then((response) => {
+          // Update shown listings
+          updateView(response);
+        })
+      );
+    } else {
+      // Get the restaurant data
+      getRestaurants(postCode).then((response) => {
+        // Update shown listings
+        updateView(response);
+      });
     }
-  });
-  // Get the restaurant data
-  getRestaurants(postCode).then((response) => {
-    console.log(response);
-    // Update shown listings
   });
 });
 
@@ -55,4 +66,45 @@ const getRestaurants = (postCode) => {
     .then((text) => {
       return text;
     });
+};
+
+const updateView = (allRestaurants) => {
+  let allRestaurantsJSON = JSON.parse(allRestaurants);
+  console.log(allRestaurants);
+  console.log(allRestaurantsJSON[0][0].RestaurantName); // Access the first object in the nested array
+  console.log("here");
+
+  for (let i = 1; i < 10; i++) {
+    // Calculate which listing data to use
+    let listingToUse = i + listingOffset - 1;
+
+    // Select the .single-listing element by it's unique ID
+    const listingElement = document.getElementById(`listing${i}`);
+
+    // Get logo element
+    const imgElement = listingElement.querySelector(".restaurant-image img");
+    // Get name element
+    const nameSpan = listingElement.querySelector(".restaurant-name span");
+    // Get rating element
+    const ratingSpan = listingElement.querySelector(".restaurant-rating span");
+    // Get cuisines element
+    const cuisinesSpan = listingElement.querySelector(
+      ".restaurant-cuisines span"
+    );
+    // Get address element
+    const addressSpan = listingElement.querySelector(
+      ".restaurant-address span"
+    );
+
+    // Update logo
+    imgElement.src = allRestaurantsJSON[0][listingToUse].LogoURL;
+    // Update name
+    nameSpan.textContent = allRestaurantsJSON[0][listingToUse].RestaurantName;
+    // Update rating
+    ratingSpan.textContent = allRestaurantsJSON[0][listingToUse].Rating;
+    // Updatecuisines
+    cuisinesSpan.textContent = allRestaurantsJSON[0][listingToUse].Cuisines;
+    // Update address
+    addressSpan.textContent = allRestaurantsJSON[0][listingToUse].Address;
+  }
 };
