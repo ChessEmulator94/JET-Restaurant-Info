511a7befeaedeeb73d12d409ec9ddafd77a6eb32
Gadi <guppiefriedman@gmail.com>
2024-04-19 12:47:38 +0200

    Full restaurant entry import added

--- server/dbHandler.js
+++ server/dbHandler.js
@@ -1,5 +1,6 @@
 import mysql from "mysql2";
 import RestaurantObj from "./restaurantObj.js";
+import { post } from "request";
 
 // Set to localhost
 const HOST = "127.0.0.1";
@@ -59,23 +60,184 @@ const fetchRestaurants = async (postCode) => {
     });
 };
 
-// Store restuarant data to database
-const saveRestaurant = (restaurantObj) => {};
-
 // Extract relevant data from API response JSON
-const storeRestaurantEssentials = async (allRestaurants) => {
+const storeRestaurantEssentials = async (allRestaurants, postCode) => {
   // Iterate over all restaurants and save data to DB
   for (let i = 0; i < allRestaurants.length; i++) {
     // Create temporary restaurantObj to hold data
+    let tempAddress = `${allRestaurants[i].address.firstLine}, ${allRestaurants[i].address.postalCode}, ${allRestaurants[i].address.city}`;
+
     let tempRestaurant = new RestaurantObj(
       allRestaurants[i].id,
       allRestaurants[i].name,
-      allRestaurants[i].cuisines,
+      allRestaurants[i].cuisines.map((cuisine) => cuisine.name),
       allRestaurants[i].rating.starRating,
       allRestaurants[i].logoUrl,
-      allRestaurants[i].address
+      tempAddress
+    );
+
+    // Remove duplicate cuisines
+    tempRestaurant.cuisines = [...new Set(tempRestaurant.cuisines)];
+
+    // Insert restaurant into db and get it's table insert id
+    const restaurantId = await insertRestaurant(tempRestaurant);
+
+    // Insert cuisines and their relationship with the restaurant
+    const cuisineIds = await insertCuisines(tempRestaurant.cuisines);
+    await insertRestaurantCuisines(restaurantId, cuisineIds);
+
+    // Insert postcodes and their relationship with the restaurant
+    const postcodeIds = await insertPostcodes(postCode);
+    await insertRestaurantPostcodes(restaurantId, postcodeIds);
+
+    console.log(
+      `Restaurant ${tempRestaurant.name} inserted with ID ${restaurantId}`
     );
   }
+  console.log("Job done");
+};
+
+// Insert row into Restaurants table
+const insertRestaurant = async (restaurantObj) => {
+  // First, check if the restaurantID already exists
+  const checkQuery = `
+    SELECT id FROM Restaurants WHERE RestaurantID = ?
+  `;
+  const [existingResult] = await pool.query(checkQuery, [restaurantObj.id]);
+
+  if (existingResult.length > 0) {
+    // If the restaurantID already exists, return its row id
+    return existingResult[0].id;
+  } else {
+    // If the restaurantID doesn't exist, insert the new restaurant and return the new row id
+    const restaurantQuery = `
+      INSERT INTO Restaurants (RestaurantName, Rating, Address, LogoURL, RestaurantID)
+      VALUES (?, ?, ?, ?, ?)
+    `;
+    const restaurantValues = [
+      restaurantObj.name,
+      restaurantObj.rating,
+      restaurantObj.address,
+      restaurantObj.logoUrl,
+      restaurantObj.id,
+    ];
+
+    const [result] = await pool.query(restaurantQuery, restaurantValues);
+    const restaurantId = result.insertId;
+    return restaurantId;
+  }
+};
+
+// Insert row into Cuisines table
+const insertCuisines = async (cuisines) => {
+  const cuisineIds = [];
+
+  for (const cuisine of cuisines) {
+    // First, check if the cuisine already exists
+    const checkQuery = `
+      SELECT id FROM Cuisines WHERE Cuisine = ?
+    `;
+    const [existingResult] = await pool.query(checkQuery, [cuisine]);
+
+    // If the cuisine already exists, add its id to the cuisineIds array
+    if (existingResult.length > 0) {
+      cuisineIds.push(existingResult[0].id);
+    } else {
+      // If the cuisine doesn't exist, insert it and add the new id to the cuisineIds array
+      const insertQuery = `
+        INSERT INTO Cuisines (Cuisine) VALUES (?)
+      `;
+      const [result] = await pool.query(insertQuery, [cuisine]);
+      cuisineIds.push(result.insertId);
+    }
+  }
+
+  return cuisineIds;
+};
+
+// Insert row into RestaurantCuisines table
+const insertRestaurantCuisines = async (restaurantId, cuisineIds) => {
+  const values = [];
+
+  for (const cuisineId of cuisineIds) {
+    // First, check if the combination of restaurantId and cuisineId already exists
+    const checkQuery = `
+      SELECT * FROM RestaurantCuisines WHERE RestaurantID = ? AND CuisineID = ?
+    `;
+    const [existingResult] = await pool.query(checkQuery, [
+      restaurantId,
+      cuisineId,
+    ]);
+
+    // If the combination doesn't exist, add it to the values array
+    if (existingResult.length === 0) {
+      values.push([restaurantId, cuisineId]);
+    }
+  }
+
+  // If there are new combinations to insert, execute the INSERT query
+  if (values.length > 0) {
+    const relationQuery = `
+      INSERT INTO RestaurantCuisines (RestaurantID, CuisineID) VALUES ?
+    `;
+    await pool.query(relationQuery, [values]);
+  }
+};
+
+// Insert row into Postcodes table
+const insertPostcodes = async (postcode) => {
+  // First, check if the postcode already exists
+  const checkQuery = `
+    SELECT id FROM PostCodes WHERE PostCode = ?
+  `;
+  const [existingResult] = await pool.query(checkQuery, [postcode]);
+
+  if (existingResult.length > 0) {
+    // If the postcode already exists, return its id
+    return [existingResult[0].id];
+  } else {
+    // If the postcode doesn't exist, insert it and return the new id
+    const insertQuery = `
+      INSERT INTO PostCodes (PostCode) VALUES (?)
+    `;
+    const [result] = await pool.query(insertQuery, [postcode]);
+    return [result.insertId];
+  }
+};
+
+// Insert row into ResaurantPostcodes table
+const insertRestaurantPostcodes = async (restaurantId, postcodeIds) => {
+  const values = [];
+
+  for (const postcodeId of postcodeIds) {
+    // First, check if the combination of restaurantId and postcodeId already exists
+    const checkQuery = `
+      SELECT * FROM RestaurantPostCodes WHERE RestaurantID = ? AND PostCodeID = ?
+    `;
+    const [existingResult] = await pool.query(checkQuery, [
+      restaurantId,
+      postcodeId,
+    ]);
+
+    if (existingResult.length === 0) {
+      // If the combination doesn't exist, add it to the values array
+      values.push([restaurantId, postcodeId]);
+    }
+  }
+
+  if (values.length > 0) {
+    // If there are new combinations to insert, execute the INSERT query
+    const relationQuery = `
+      INSERT INTO RestaurantPostCodes (RestaurantID, PostCodeID) VALUES ?
+    `;
+    await pool.query(relationQuery, [values]);
+  }
+};
+
+export const addListings = async (postCode) => {
+  fetchRestaurants(postCode).then((result) => {
+    storeRestaurantEssentials(result, postCode);
+  });
 };
 
 // Test function
@@ -84,7 +246,8 @@ getRestaurants("24680").then((result) => {
 });
 
 // Test fetchRestaurants
+//"EC1A9LH"
+//"EC4M7RF"
 fetchRestaurants("EC4M7RF").then((result) => {
-  //console.log(result);
-  storeRestaurantEssentials(result);
+  storeRestaurantEssentials(result, "EC4M7RF");
 });
