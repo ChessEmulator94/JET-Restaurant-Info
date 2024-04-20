9d5c710b289c7b1fe151cae0f0e3ffaa2e454fc6
Gadi <guppiefriedman@gmail.com>
2024-04-20 16:17:27 +0200

    Refactoring / Clearer Commenting

--- web_page/scripts/indexScript.js
+++ web_page/scripts/indexScript.js
@@ -1,19 +1,23 @@
+// Get elements from the DOM
 const searchButton = document.querySelector(".searchButton");
 const searchBar = document.getElementById("searchBar");
 
-// Set equal to the URL server is running on
+// Set the db server URL
 const SERVER_URL = `http://localhost:5500`;
 
+// Add onClick function to search button
 searchButton.addEventListener("click", () => {
   // Get post code from screen
   let postCode = document.getElementById("searchBar").value;
+  // Reset the search bar
   searchBar.value = "";
 
+  // Check that postcode is valid
   if (validatePostCode(postCode)) {
     const nextPage = `./searchResultsView.html?postCode=${encodeURIComponent(
       postCode
     )}`;
-
+    // Load results window
     window.location.href = nextPage;
   }
 });
