f9e06c057e2c3ca3bfa52691021d2847403ca281
Gadi <guppiefriedman@gmail.com>
2024-04-21 01:56:35 +0200

    Index style and format completely overhauled

--- web_page/scripts/indexScript.js
+++ web_page/scripts/indexScript.js
@@ -1,6 +1,6 @@
 // Get elements from the DOM
-const searchButton = document.querySelector(".searchButton");
-const searchBar = document.getElementById("searchBar");
+const searchButton = document.querySelector(".search-btn");
+const searchBar = document.querySelector(".search-input");
 
 // Set the db server URL
 const SERVER_URL = `http://localhost:5500`;
@@ -8,10 +8,9 @@ const SERVER_URL = `http://localhost:5500`;
 // Add onClick function to search button
 searchButton.addEventListener("click", () => {
   // Get post code from screen
-  let postCode = document.getElementById("searchBar").value;
+  let postCode = searchBar.value;
   // Reset the search bar
   searchBar.value = "";
-
   // Check that postcode is valid
   if (validatePostCode(postCode)) {
     const nextPage = `./searchResultsView.html?postCode=${encodeURIComponent(
