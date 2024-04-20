141422d60288b85d5603e65d6d20b77b53c7f0c0
Gadi <guppiefriedman@gmail.com>
2024-04-20 16:11:58 +0200

    Added functionality to update view based on provided restaurant id

--- web_page/scripts/restaurantInfoScript.js
+++ web_page/scripts/restaurantInfoScript.js
@@ -3,6 +3,8 @@ const SERVER_URL = `http://localhost:5500`;
 
 let restaurant;
 
+let globalPostCode;
+
 // Call dbHandler.js getRestaurantInfo() via the server
 const getRestaurantInfo = (id) => {
   let queryURL = `${SERVER_URL}/restaurant/${id}`;
@@ -13,17 +15,44 @@ const getRestaurantInfo = (id) => {
     });
 };
 
+const locationMapIframe = document.querySelector(".restaurantPin");
+const restaurantLogo = document.querySelector(".logo");
+const restaurantName = document.querySelector(".restaurant-name span");
+const restaurantRating = document.querySelector(".rating span");
+const restaurantCuisines = document.querySelector(".cuisines span");
+const restaurantAddress = document.querySelector(".address span");
+
 // On load of webpage
 document.addEventListener("DOMContentLoaded", () => {
   // Get the value of the "id" query parameter from the URL
   const urlParams = new URLSearchParams(window.location.search);
   const restaurantID = urlParams.get("id");
+  globalPostCode = urlParams.get("postcode");
 
   getRestaurantInfo(restaurantID).then((response) => {
     const restaurant = JSON.parse(response);
-    // Get the <iframe> element
-    const locationMapIframe = document.querySelector(".restaurantPin");
-    // Update the src attribute of the <iframe>
-    locationMapIframe.src = `https://www.google.com/maps?q=[${restaurant.Address}]&output=embed`;
+    updateDisplayedRestaurant(restaurant);
   });
 });
+
+const updateDisplayedRestaurant = (restaurant) => {
+  console.log(restaurant);
+  // Update logo
+  restaurantLogo.style.backgroundImage = `url(${restaurant.LogoURL})`;
+
+  // Update name
+  restaurantName.textContent = restaurant.RestaurantName;
+
+  // Update rating
+  restaurantRating.textContent = `Rating ${restaurant.Rating}`;
+
+  // Update cuisines
+  restaurantCuisines.textContent = restaurant.cuisines.join(" | ");
+
+  // Update address
+  restaurantAddress.textContent = restaurant.Address;
+
+  // Get the <iframe> element
+  // Update the src attribute of the <iframe>
+  locationMapIframe.src = `https://www.google.com/maps?q=[${restaurant.Address}]&output=embed`;
+};
