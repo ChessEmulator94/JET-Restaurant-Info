957186cf7d05f07b3341788842883fea2ba19d39
Gadi <guppiefriedman@gmail.com>
2024-04-20 21:13:40 +0200

    Ability to cycle through restaurants using navigation buttons added

--- web_page/scripts/restaurantInfoScript.js
+++ web_page/scripts/restaurantInfoScript.js
@@ -1,9 +1,46 @@
 // Change based on actual server URL
 const SERVER_URL = `http://localhost:5500`;
 
+let allRestaurantsJSON;
 let restaurant;
-
+let restaurantPositionInJSON;
 let globalPostCode;
+
+/* GET DOM ELEMENTS */
+const locationMapIframe = document.querySelector(".restaurantPin");
+const restaurantLogo = document.querySelector(".logo");
+const restaurantName = document.querySelector(".restaurant-name span");
+const restaurantRating = document.querySelector(".rating span");
+const restaurantCuisines = document.querySelector(".cuisines span");
+const restaurantAddress = document.querySelector(".address span");
+const leftNavBtn = document.getElementById("nav-left");
+const rightNavBtn = document.getElementById("nav-right");
+
+leftNavBtn.addEventListener("click", () => {
+  changeRestaurant(-1);
+  updateDisplayedRestaurant(allRestaurantsJSON[0][restaurantPositionInJSON]);
+});
+
+rightNavBtn.addEventListener("click", () => {
+  changeRestaurant(1);
+  console.log(allRestaurantsJSON[0][restaurantPositionInJSON].id);
+  updateDisplayedRestaurant(allRestaurantsJSON[0][restaurantPositionInJSON]);
+});
+
+// On load of webpage
+document.addEventListener("DOMContentLoaded", () => {
+  // Get the value of the "id" query parameter from the URL
+  const urlParams = new URLSearchParams(window.location.search);
+  const restaurantID = urlParams.get("id");
+  // Get the value of the "postcode" query parameter from the URL
+  globalPostCode = urlParams.get("postcode");
+
+  getRestaurantInfo(restaurantID).then((response) => {
+    const restaurant = JSON.parse(response);
+    updateDisplayedRestaurant(restaurant);
+    findRestaurantPosition(restaurantID, globalPostCode);
+  });
+});
 
 // Call dbHandler.js getRestaurantInfo() via the server
 const getRestaurantInfo = (id) => {
@@ -15,28 +52,53 @@ const getRestaurantInfo = (id) => {
     });
 };
 
-const locationMapIframe = document.querySelector(".restaurantPin");
-const restaurantLogo = document.querySelector(".logo");
-const restaurantName = document.querySelector(".restaurant-name span");
-const restaurantRating = document.querySelector(".rating span");
-const restaurantCuisines = document.querySelector(".cuisines span");
-const restaurantAddress = document.querySelector(".address span");
+// Call dbHandler.js getRestaurants() via server
+const getRestaurants = (postCode) => {
+  let queryURL = `${SERVER_URL}/restaurants/${postCode}`;
+  return fetch(queryURL, {})
+    .then((response) => response.text())
+    .then((text) => {
+      return text; // All restaurants association with postCode
+    });
+};
 
-// On load of webpage
-document.addEventListener("DOMContentLoaded", () => {
-  // Get the value of the "id" query parameter from the URL
-  const urlParams = new URLSearchParams(window.location.search);
-  const restaurantID = urlParams.get("id");
-  globalPostCode = urlParams.get("postcode");
+// Changes the displayed restaurant forward or backwards
+const changeRestaurant = (direction) => {
+  if (direction == 1) {
+    // Direction (right)
+    // Show next listing or listing[0] if at last listing
+    if (restaurantPositionInJSON == allRestaurantsJSON[0].length - 1) {
+      restaurantPositionInJSON = 0;
+    } else {
+      restaurantPositionInJSON += 1;
+    }
+  } else {
+    // Direction == -1 (left)
+    // Show prev listing or last if at listing 0
+    if (restaurantPositionInJSON == 0) {
+      restaurantPositionInJSON = allRestaurantsJSON[0].length;
+    } else {
+      restaurantPositionInJSON -= 1;
+    }
+  }
+};
 
-  getRestaurantInfo(restaurantID).then((response) => {
-    const restaurant = JSON.parse(response);
-    updateDisplayedRestaurant(restaurant);
+// Determines which restaurant was selected
+const findRestaurantPosition = (restaurantID, postCode) => {
+  // Get all restaurants associated with postCode
+  getRestaurants(postCode).then((response) => {
+    let allRestaurants = response;
+    console.log(allRestaurants);
+    // Get JSON of restaurants
+    allRestaurantsJSON = JSON.parse(allRestaurants);
+    // Get the index of thte restaurant within the JSON
+    restaurantPositionInJSON = allRestaurantsJSON[0].findIndex(
+      (restaurant) => restaurant.id == restaurantID
+    );
   });
-});
+};
 
 const updateDisplayedRestaurant = (restaurant) => {
-  console.log(restaurant);
   // Update logo
   restaurantLogo.style.backgroundImage = `url(${restaurant.LogoURL})`;
 
@@ -46,8 +108,11 @@ const updateDisplayedRestaurant = (restaurant) => {
   // Update rating
   restaurantRating.textContent = `Rating ${restaurant.Rating}`;
 
-  // Update cuisines
-  restaurantCuisines.textContent = restaurant.cuisines.join(" | ");
+  if (restaurant.hasOwnProperty("Cuisines")) {
+    restaurantCuisines.textContent = restaurant.Cuisines;
+  } else if (restaurant.hasOwnProperty("cuisines")) {
+    restaurantCuisines.textContent = restaurant.cuisines.join(" | ");
+  }
 
   // Update address
   restaurantAddress.textContent = restaurant.Address;
