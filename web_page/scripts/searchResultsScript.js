d7fe0f7b0849cc67d3babc339cb6e18806a84fed
Gadi <guppiefriedman@gmail.com>
2024-04-20 16:58:35 +0200

    Refactoring / Clearer Commenting

--- web_page/scripts/searchResultsScript.js
+++ web_page/scripts/searchResultsScript.js
@@ -1,21 +1,82 @@
-// Set equal to the URL server is running on (see server.js)
-const SERVER_URL = `http://localhost:5500`;
+/* GLOBAL VARIABLES */
+const SERVER_URL = `http://localhost:5500`; // Set the db server URL
+let listingOffset = 0; // Offset for restaurants being shown calculation
+let allRestaurantsLong; // Stores all restaurants from a search
+let globalPostCode; // Store postCode of current search
 
-// Offset the restaurants being shown
-let listingOffset = 0;
-
-// Stores all restaurants from a search
-let allRestaurantsLong;
-
-// Store postCode of current search
-let globalPostCode;
-
-// Get elements from the DOM
+/* GET ELEMENTS FROM DOM */
+// Address
 const displayedArea = document.querySelector(".location-name span");
+// Navigation Buttons
 const leftArrow = document.querySelector("#nav-btn-1");
 const rightArrow = document.querySelector("#nav-btn-2");
+// Listings
+const listing1 = document.getElementById("listing1");
+const listing2 = document.getElementById("listing2");
+const listing3 = document.getElementById("listing3");
+const listing4 = document.getElementById("listing4");
+const listing5 = document.getElementById("listing5");
+const listing6 = document.getElementById("listing6");
+const listing7 = document.getElementById("listing7");
+const listing8 = document.getElementById("listing8");
+const listing9 = document.getElementById("listing9");
 
-/* When window loads, do thte following:
+/* EVENT LISTENERS */
+// View previous restaurants
+leftArrow.addEventListener("click", () => {
+  viewNextRestaurants(-9);
+});
+// View next restaurants
+rightArrow.addEventListener("click", () => {
+  viewNextRestaurants(9);
+});
+// Show listing 1 info
+listing1.addEventListener("click", () => {
+  let selectedRestaurantID = listing1.dataset.restaurantID;
+  loadNextScreen(selectedRestaurantID);
+});
+// Show listing 2 info
+listing2.addEventListener("click", () => {
+  let selectedRestaurantID = listing2.dataset.restaurantID;
+  loadNextScreen(selectedRestaurantID);
+});
+// Show listing 3 info
+listing3.addEventListener("click", () => {
+  let selectedRestaurantID = listing3.dataset.restaurantID;
+  loadNextScreen(selectedRestaurantID);
+});
+// Show listing 4 info
+listing4.addEventListener("click", () => {
+  let selectedRestaurantID = listing4.dataset.restaurantID;
+  loadNextScreen(selectedRestaurantID);
+});
+// Show listing 5 info
+listing5.addEventListener("click", () => {
+  let selectedRestaurantID = listing5.dataset.restaurantID;
+  loadNextScreen(selectedRestaurantID);
+});
+// Show listing 6 info
+listing6.addEventListener("click", () => {
+  let selectedRestaurantID = listing6.dataset.restaurantID;
+  loadNextScreen(selectedRestaurantID);
+});
+// Show listing 7 info
+listing7.addEventListener("click", () => {
+  let selectedRestaurantID = listing7.dataset.restaurantID;
+  loadNextScreen(selectedRestaurantID);
+});
+// Show listing 8 info
+listing8.addEventListener("click", () => {
+  let selectedRestaurantID = listing8.dataset.restaurantID;
+  loadNextScreen(selectedRestaurantID);
+});
+// Show listing 9 info
+listing9.addEventListener("click", () => {
+  let selectedRestaurantID = listing9.dataset.restaurantID;
+  loadNextScreen(selectedRestaurantID);
+});
+
+/* When window loads, do the following:
  *  - Extract searched postcode from the URL
  *  - Check if the postcode is in the database already
  *      - If not in db, add it + associated restaurants to the db
@@ -26,26 +87,26 @@ document.addEventListener("DOMContentLoaded", () => {
   // Get the value of the "postCode" query parameter from the URL
   const urlParams = new URLSearchParams(window.location.search);
   const postCode = urlParams.get("postCode");
-
   globalPostCode = postCode;
-
+  // Update the displayed area
   updateAreaPin(postCode);
-
+  // Check if restaurants association with postCode are in db yet or not
   checkPostcodeExists(postCode).then((exists) => {
     if (exists === "false") {
-      console.log("Data doesn't exist yet");
+      // Add the restaurants to the db
       addRestaurantsToDb(postCode)
         .then(() => {
           // After addRestaurantsToDb finishes, get the restaurant data
           return getRestaurants(postCode);
         })
         .then((response) => {
-          // After getRestaurants finishes, update the view
+          // After getRestaurants finishes, update the display
           allRestaurantsLong = response;
           updateView(response);
         });
     } else {
       getRestaurants(postCode).then((response) => {
+        // Update the display
         allRestaurantsLong = response;
         updateView(response);
       });
@@ -53,92 +114,76 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// Call dbHandler.js checkPostcodeExists() via the server
+/* SERVER REQUEST FUNCTIONS */
+// Call dbHandler.js checkPostcodeExists() via server
 const checkPostcodeExists = (postCode) => {
   let queryURL = `${SERVER_URL}/checkExists/${postCode}`;
   return fetch(queryURL, {})
     .then((response) => response.text())
     .then((text) => {
-      return text;
+      return text; // true or false
     });
 };
-
-// Call JET API to update the db
+// Call dbHandler.js addListings() via server
 const addRestaurantsToDb = (postCode) => {
   let queryURL = `${SERVER_URL}/add-restaurants/${postCode}`;
   return fetch(queryURL, {})
     .then((response) => response.text())
     .then((text) => {
-      return text;
+      return text; // All added restaurants associated with postCode
     });
 };
-
-// Call dbHandler.js getRestaurants() via the server
+// Call dbHandler.js getRestaurants() via server
 const getRestaurants = (postCode) => {
   let queryURL = `${SERVER_URL}/restaurants/${postCode}`;
   return fetch(queryURL, {})
     .then((response) => response.text())
     .then((text) => {
-      return text;
+      return text; // All restaurants association with postCode
     });
 };
 
+/* GENERAL FUNCTIONS */
+// Change grid elements to show restaurant data
 const updateView = (allRestaurants) => {
+  // Get JSON or restaurants
   let allRestaurantsJSON = JSON.parse(allRestaurants);
-
+  // Update the grid
   for (let i = 1; i < 10; i++) {
     // Calculate which listing data to use
     let listingToUse = i + listingOffset - 1;
-
     // Store the restaurant id
     let restID = allRestaurantsJSON[0][listingToUse].id;
-
-    // Select the .single-listing element by it's unique ID
+    // Get elements from the DOM
     const listingElement = document.getElementById(`listing${i}`);
-
-    // Get logo element
     const imgElement = listingElement.querySelector(".restaurant-image img");
-    // Get name element
     const nameSpan = listingElement.querySelector(".restaurant-name span");
-    // Get rating element
     const ratingSpan = listingElement.querySelector(".restaurant-rating span");
-    // Get cuisines element
     const cuisinesSpan = listingElement.querySelector(
       ".restaurant-cuisines span"
     );
-    // Get address element
     const addressSpan = listingElement.querySelector(
       ".restaurant-address span"
     );
-
-    // Get the first cuisine from the array
     const cuisineArray =
       allRestaurantsJSON[0][listingToUse].Cuisines.split(" | ");
-
-    // Use the first valid cuisine image as the background
+    // Update the elements
+    listingElement.dataset.restaurantID = restID;
+    imgElement.src = allRestaurantsJSON[0][listingToUse].LogoURL;
+    nameSpan.textContent = allRestaurantsJSON[0][listingToUse].RestaurantName;
+    ratingSpan.textContent = allRestaurantsJSON[0][listingToUse].Rating;
+    cuisinesSpan.textContent = allRestaurantsJSON[0][listingToUse].Cuisines;
+    addressSpan.textContent = allRestaurantsJSON[0][listingToUse].Address;
+    // Get a valid cuisine image and updatet the background food image
     getFirstValidCuisineImage(cuisineArray, restID).then((imageUrl) => {
       listingElement.style.backgroundImage = `url('${imageUrl}')`;
     });
-
-    // Update restaurant id
-    listingElement.dataset.restaurantID = restID;
-    // Update logo
-    imgElement.src = allRestaurantsJSON[0][listingToUse].LogoURL;
-    // Update name
-    nameSpan.textContent = allRestaurantsJSON[0][listingToUse].RestaurantName;
-    // Update rating
-    ratingSpan.textContent = allRestaurantsJSON[0][listingToUse].Rating;
-    // Updatecuisines
-    cuisinesSpan.textContent = allRestaurantsJSON[0][listingToUse].Cuisines;
-    // Update address
-    addressSpan.textContent = allRestaurantsJSON[0][listingToUse].Address;
   }
-
   // Disable the navigation buttons if necessary
   checkNavigationButtons();
 };
 
-// Function to check if the image URL returns a 404
+// Checks if an image URL returns a 404
 const isImageOk = (url) => {
   return new Promise((resolve, reject) => {
     const img = new Image();
@@ -148,7 +193,7 @@ const isImageOk = (url) => {
   });
 };
 
-// Function to find the first valid cuisine image
+// Finds the first valid cuisine image from an array of cuisines
 const getFirstValidCuisineImage = (cuisineArray, restID) => {
   const findValidImage = async () => {
     for (const cuisine of cuisineArray) {
@@ -164,27 +209,22 @@ const getFirstValidCuisineImage = (cuisineArray, restID) => {
     // If no valid cuisine image is found, return a default image URL
     return "https://d15shllkswkct0.cloudfront.net/wp-content/blogs.dir/1/files/2022/04/Image-8.png";
   };
-
   return findValidImage();
 };
 
+// Updates the location next to the area pin
 const updateAreaPin = (postCode) => {
   displayedArea.textContent = postCode;
 };
-
+// Displays the previous or next restaurants (9)
 const viewNextRestaurants = (direction) => {
   listingOffset += direction;
   updateView(allRestaurantsLong);
 };
 
-leftArrow.addEventListener("click", () => {
-  viewNextRestaurants(-9);
-});
-rightArrow.addEventListener("click", () => {
-  viewNextRestaurants(9);
-});
-
+// Check if the navigation buttons need to be disabled/enabled
 const checkNavigationButtons = () => {
+  // LEFT BUTTON
   if (listingOffset == 0) {
     // Disable left navigation button
     leftArrow.style.pointerEvents = "none";
@@ -196,84 +236,10 @@ const checkNavigationButtons = () => {
     leftArrow.style.opacity = "1";
     leftArrow.style.cursor = "pointer";
   }
-
-  // Add functionality to disable right arrow if at end
+  // TODO: Add check for right button at end of list
 };
-// Add event listeners for listings
-const listing1 = document.getElementById("listing1");
-const listing2 = document.getElementById("listing2");
-const listing3 = document.getElementById("listing3");
-const listing4 = document.getElementById("listing4");
-const listing5 = document.getElementById("listing5");
-const listing6 = document.getElementById("listing6");
-const listing7 = document.getElementById("listing7");
-const listing8 = document.getElementById("listing8");
-const listing9 = document.getElementById("listing9");
 
-listing1.addEventListener("click", () => {
-  // Get the restaurant that was clicked
-  let selectedRestaurantID = listing1.dataset.restaurantID;
-  // Load next screen with restaurant info
-  loadNextScreen(selectedRestaurantID);
-});
-
-listing2.addEventListener("click", () => {
-  // Get the restaurant that was clicked
-  let selectedRestaurantID = listing2.dataset.restaurantID;
-  // Load next screen with restaurant info
-  loadNextScreen(selectedRestaurantID);
-});
-
-listing3.addEventListener("click", () => {
-  // Get the restaurant that was clicked
-  let selectedRestaurantID = listing3.dataset.restaurantID;
-  // Load next screen with restaurant info
-  loadNextScreen(selectedRestaurantID);
-});
-
-listing4.addEventListener("click", () => {
-  // Get the restaurant that was clicked
-  let selectedRestaurantID = listing4.dataset.restaurantID;
-  // Load next screen with restaurant info
-  loadNextScreen(selectedRestaurantID);
-});
-
-listing5.addEventListener("click", () => {
-  // Get the restaurant that was clicked
-  let selectedRestaurantID = listing5.dataset.restaurantID;
-  // Load next screen with restaurant info
-  loadNextScreen(selectedRestaurantID);
-});
-
-listing6.addEventListener("click", () => {
-  // Get the restaurant that was clicked
-  let selectedRestaurantID = listing6.dataset.restaurantID;
-  // Load next screen with restaurant info
-  loadNextScreen(selectedRestaurantID);
-});
-
-listing7.addEventListener("click", () => {
-  // Get the restaurant that was clicked
-  let selectedRestaurantID = listing7.dataset.restaurantID;
-  // Load next screen with restaurant info
-  loadNextScreen(selectedRestaurantID);
-});
-
-listing8.addEventListener("click", () => {
-  // Get the restaurant that was clicked
-  let selectedRestaurantID = listing8.dataset.restaurantID;
-  // Load next screen with restaurant info
-  loadNextScreen(selectedRestaurantID);
-});
-
-listing9.addEventListener("click", () => {
-  // Get the restaurant that was clicked
-  let selectedRestaurantID = listing9.dataset.restaurantID;
-  // Load next screen with restaurant info
-  loadNextScreen(selectedRestaurantID);
-});
-
-// Load restaurant info screen and pass it the restaurant id
+// Load restaurantInfoView with the selected restaurant's ID
 const loadNextScreen = (selectedRestaurantID) => {
   const nextPage = `./restaurantInfoView.html?id=${encodeURIComponent(
     selectedRestaurantID
