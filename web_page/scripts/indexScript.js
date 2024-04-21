f64cc99fe9d22a4113b59e27d3ddfc095f09a7ba
Gadi <guppiefriedman@gmail.com>
2024-04-21 15:32:44 +0200

    Postcode validation functionality added; Functionality to search on enter key press added

--- web_page/scripts/indexScript.js
+++ web_page/scripts/indexScript.js
@@ -7,24 +7,39 @@ const SERVER_URL = `http://localhost:5500`;
 
 // Add onClick function to search button
 searchButton.addEventListener("click", () => {
+  submitSearch();
+});
+
+searchBar.addEventListener("keyup", (event) => {
+  // If the Enter key was pressed
+  if (event.key === "Enter") {
+    submitSearch();
+  }
+});
+
+const submitSearch = () => {
   // Get post code from screen
   let postCode = searchBar.value;
   // Reset the search bar
   searchBar.value = "";
   // Check that postcode is valid
   if (validatePostCode(postCode)) {
+    // Remove whitespace
+    postCode = postCode.split(" ").join("");
     const nextPage = `./searchResultsView.html?postCode=${encodeURIComponent(
       postCode
     )}`;
+
     // Load results window
     window.location.href = nextPage;
+  } else {
+    console.log("Invalid postcode");
   }
-});
+};
 
-// Need to implement and move to different file
+// Validates if postCode provided follows correct format
 const validatePostCode = (postCode) => {
   // Check if postcode exists
-  // const ukPostcodeRegex = /^([A-Z]{1,2}[0-9][A-Z0-9]?)\s*([0-9][A-Z]{2})$/i;
-  // return ukPostcodeRegex.test(postCode);
-  return true;
+  const ukPostcodeRegex = /^([A-Z]{1,2}[0-9][A-Z0-9]?)\s*([0-9][A-Z]{2})$/i;
+  return ukPostcodeRegex.test(postCode);
 };
